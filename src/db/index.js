f9ecea51390155b410974077271a36ed639d8041
korovnmm <66383716+korovnmm@users.noreply.github.com>
2021-11-22 03:59:40 -0800

    in-progress attempts at getting MUI datagrid to update with firestore data on the Tickets page

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,15 @@
 import { db } from '../firebase'
-import { useCollectionData } from 'react-firebase-hooks'
+import {
+    collection, 
+    doc, 
+    getDoc,
+    getDocs,
+    query,
+    where 
+} from 'firebase/firestore'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+var cache = {}
 
 /**
  * Retrieves JSON object info of a single ticket from the database
@@ -11,8 +21,6 @@ export const getTicketByCode = async (code) => {
 }
 
 export const getTicketsByPrefix = async (prefix) => {
-    throw { name: "NotImplementedError", message: "function not implemented yet!" };
-
     var len = prefix.length;
     var head = prefix.slice(0, len-1);
     var tail = prefix.slice(len-1, len);
@@ -20,9 +28,47 @@ export const getTicketsByPrefix = async (prefix) => {
     var start = prefix;
     var stop = head + String.fromCharCode(tail.charCodeAt(0) + 1);
 
-    const query = db.collection("ticket-info")
-            .where(prefix, '>=', start)
-            .where(prefix, '<', stop);
-    
-    return;
+    const ticketsRef = collection(db, "ticket-info");
+    const q = query(ticketsRef,
+            where("__name__", '>=', start),
+            where("__name__", '<', stop));
+    const snap = await getDocs(q);
+
+    var data = [];
+    snap.forEach( (doc) => {
+        var d = doc.data();
+        d.id = data.length+1;
+        d.code = doc.id;
+        data.push(d);
+    });
+    console.log("update");
+    return data;
+}
+
+export const getUserShopTag = async (uid) => {
+    var tag;
+    if (cache["shopTag"]) {
+        tag = cache["shopTag"]
+        console.log("retrieved from cache");
+    } else {
+        var ref = doc(db, "users", uid);
+        var snapshot = await getDoc(ref);
+
+        if (snapshot.exists()) {
+            tag = snapshot.data().shopTag;
+            cache["shopTag"] = tag;
+        } else {
+            alert("No shop tag set!");
+            tag = null;
+        }
+            
+        console.log("retrieved from querying");
+    }    
+
+    return tag;
+}
+
+export const getTicketsGeneratedByUser = async (user) => {
+    var prefix = await getUserShopTag(user.uid);
+    return getTicketsByPrefix(prefix);
 }
--- src/pages/dashboard/Tickets.js
+++ src/pages/dashboard/Tickets.js
@@ -1,5 +1,5 @@
 // DataGrid Documentation: https://mui.com/api/data-grid/data-grid/#component-name
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { 
     Autocomplete,
     Box,
@@ -9,25 +9,40 @@ import {
 import { DataGrid } from '@mui/x-data-grid'
 
 import { generateTickets } from 'api'
+import { useAuthState } from 'auth'
+import { getTicketsGeneratedByUser } from 'db';
+import { useState } from 'react'
 
 const columns = [
-    { field: 'orderID', headerName: 'Order #', width: 100 },
+    //{ field: 'orderID', headerName: 'Order #', width: 100 },
     { field: 'email', headerName: 'Email Address', width: 170 },
     { field: 'code', headerName: 'Play Code', width: 170 },
-    { field: 'prizeName', headerName: 'Prize', width: 150 },
-    { field: 'memo', headerName: 'Memo', width: 170 },
-    { field: 'shipped', headerName: 'Shipped?', width: 150, type: "boolean" },
+    //{ field: 'prizeID', headerName: 'Prize', width: 150 },
+    //{ field: 'memo', headerName: 'Memo', width: 170 },
+    //{ field: 'shipped', headerName: 'Shipped?', width: 150, type: "boolean" },
+    //{ field: 'redeemed', headerName: 'Redeemed?', width: 150, type: "boolean" },
+    //{ field: 'createdAt', headerName: 'Time Generated', width: 150, type: "dateTime", valueGetter: ({value}) => value && new Date(value.seconds) }
 ];
 
-const rows = [
+/*
+[
     {id: 1, email: "123@abc.com", orderID:"QWER-1357", code:"shopname-0A0A0A", prizeName: "Prize A", memo: "", shipped: true},
     { id: 2, email: "321@abc.com", orderID: "ASDF-2468", code: "shopname-1B1B1B", prizeName: "Prize B", memo: "ship after thanksgiving", shipped: false }
 ];
-
+*/
 const amountOptions = Array.from({length:10}, (v,k)=>k+1); // a list from 1 to 10
 
 
 export const Tickets = () => {
+    const { user } = useAuthState();
+    const [rows, setRows] = useState([]);
+    /*
+    useEffect(() => {
+        var tickets = getTicketsGeneratedByUser(user);
+        setRows(tickets);
+    }, [rows]);
+    useEffect(() => { console.log(rows)}, [rows]);
+    */
     const sendGenerateTicketsRequest = useCallback(async e => {
         e.preventDefault();
         
@@ -72,6 +87,7 @@ export const Tickets = () => {
                 />
                 <Button variant="contained" type="submit">Generate New Code</Button>
             </Box>
+            <div>{JSON.stringify(rows)}</div>
         </>
     );
 }
