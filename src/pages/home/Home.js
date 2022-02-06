a868ddb0b3064d24e0a0ff36e8b414633f194d97
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-02-06 05:10:17 -0800

    refactor(home): fixed some spacing and indent issues

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -16,15 +16,13 @@ import {
 
 import { Link } from 'react-router-dom'
 import { useCallback } from "react";
-  
-    
       
 
 export const Home = () => {
 
     const location = useHistory();
     const handleClick = useCallback(async e => {
-        e.preventDefault();
+        e.preventDefault(); // prevents auto-refresh
 
         const {shopTag, digitKey} = e.target.elements;   
         console.log(shopTag.value, digitKey.value);
@@ -33,82 +31,71 @@ export const Home = () => {
     
     return (
         <>
-    <Container maxWidth="lg"
-        
-        >
-        <div>
-            
-            <Box component="center"
-            sx={{
-                '& > :not(style)': { m: 1, width: '40ch',height: '20ch' }
-                ,
+            <Container maxWidth="lg">
+            <div>
+                <Box component="center"
+                    sx={{
+                        '& > :not(style)': { m: 1, width: '40ch', height: '20ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >  
+                    <Box component="form" onSubmit ={handleClick} 
+                        sx={{
+                            boxShadow: 3,
+                            bgcolor: 'background.paper',
                 
-            }}
-            noValidate
-            autoComplete="off"
-            >  
-                <Box component="form" onSubmit ={handleClick} 
-                sx={{
-                    boxShadow: 3,
-                    bgcolor: 'background.paper',
-        
-                    color: 'black',
-                    p: 4,
-                    m: 2,
-                    borderRadius: 4,
-            
+                            color: 'black',
+                            p: 4,
+                            m: 2,
+                            borderRadius: 4,
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >   
+                        
+                        <Typography variant="h2" component="h2" > Welcome!</Typography>
+                        <Typography variant="body" component="body" align="center"> Please Enter Play Code</Typography>
+                        <TextField required id="shopTag" type="shopTag" label="Shop Tag" autoComplete="off"/>
+                        <TextField required id="digitKey" type="digitKey" label="Digit Key" autoComplete="off" />                
+                        <Stack spacing={10} direction="column">
                     
-                }}
+                        <Button variant="outlined" color ="secondary" type="submit"  >Enter</Button>
+                        <Typography variant="overline" component="overline" align="center"> (No account needed to play)</Typography>
+                        
+                        </Stack>
 
-                noValidate
-                autoComplete="off"
-                >   
-                    
-                    <Typography variant="h2" component="h2" > Welcome!</Typography>
-                    <Typography variant="body" component="body" align="center"> Please Enter Play Code</Typography>
-                    <TextField required id="shopTag" type="shopTag" label="Shop Tag" autoComplete="off"/>
-                    <TextField required id="digitKey" type="digitKey" label="Digit Key" autoComplete="off" />                
-                    <Stack spacing={10} direction="column">
-                
-                    <Button variant="outlined" color ="secondary" type="submit"  >Enter</Button>
-                    <Typography variant="overline" component="overline" align="center"> (No account needed to play)</Typography>
-                    
-                    </Stack>
-            
-
+                    </Box>
                 </Box>
-            </Box>
-            
-        </div>
-        <Grid container justifyContent="space-between"> 
-    <Box
-        component="img"
-        sx={{
-            justifyContent: 'flex-start',
-            height: 500,
-            width: 300,
-            maxHeight: { xs: 500, md: 250
-            },
-            maxWidth: { xs: 300, md: 150 },
-            }}
-        src= {greenlogo}
-    />
-    
-        <Box 
-        component="img"
-        sx={{
-            height: 500,
-            width: 300,
-            maxHeight: { xs: 500, md: 250
-            },
-            maxWidth: { xs: 300, md: 150 },
-            }}
-        src= {pinklogo}
-    />
-    </Grid>
-    </Container>
-    
-    
+            </div>
+
+            <Grid container justifyContent="space-between"> 
+            <Box
+                component="img"
+                sx={{
+                    justifyContent: 'flex-start',
+                    height: 500,
+                    width: 300,
+                    maxHeight: { xs: 500, md: 250
+                    },
+                    maxWidth: { xs: 300, md: 150 },
+                    }}
+                src= {greenlogo}
+            />
+        
+            <Box 
+            component="img"
+            sx={{
+                height: 500,
+                width: 300,
+                maxHeight: { xs: 500, md: 250
+                },
+                maxWidth: { xs: 300, md: 150 },
+                }}
+            src= {pinklogo}
+            />
+            </Grid>
+            </Container>
         </>
     )
 }
