653c9073f470baf1ed745c0f707ac7f6d985a673
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-02-06 05:54:39 -0800

    refactor(home): created a FormContainer component to move out clutter from the Box components inside Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -16,58 +16,63 @@ import {
 
 import { Link } from 'react-router-dom'
 import { useCallback } from "react";
-      
 
-export const Home = () => {
 
+export const FormContainer = ({...props}) => {
     const location = useHistory();
+    
     const handleClick = useCallback(async e => {
         e.preventDefault(); // prevents auto-refresh
 
-        const {shopTag, digitKey} = e.target.elements;   
+        const { shopTag, digitKey } = e.target.elements;
         console.log(shopTag.value, digitKey.value);
         location.push(`/redeem/${shopTag.value}/${digitKey.value}`);
     }, [location]);
-    
+
     return (
-        <>
-            <Container maxWidth="lg">
-            <div>
-                <Box component="center"
+        <div>
+            <Box component="center"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '40ch', height: '20ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <Box {...props} component="form" onSubmit={handleClick}
                     sx={{
-                        '& > :not(style)': { m: 1, width: '40ch', height: '20ch' },
+                        boxShadow: 3,
+                        bgcolor: 'background.paper',
+
+                        color: 'black',
+                        p: 4,
+                        m: 2,
+                        borderRadius: 4,
                     }}
                     noValidate
                     autoComplete="off"
-                >  
-                    <Box component="form" onSubmit ={handleClick} 
-                        sx={{
-                            boxShadow: 3,
-                            bgcolor: 'background.paper',
-                
-                            color: 'black',
-                            p: 4,
-                            m: 2,
-                            borderRadius: 4,
-                        }}
-                        noValidate
-                        autoComplete="off"
-                    >   
-                        
-                        <Typography variant="h2" component="h2" > Welcome!</Typography>
-                        <Typography variant="body" component="body" align="center"> Please Enter Play Code</Typography>
-                        <TextField required id="shopTag" type="shopTag" label="Shop Tag" autoComplete="off"/>
-                        <TextField required id="digitKey" type="digitKey" label="Digit Key" autoComplete="off" />                
-                        <Stack spacing={10} direction="column">
-                    
-                        <Button variant="outlined" color ="secondary" type="submit"  >Enter</Button>
-                        <Typography variant="overline" component="overline" align="center"> (No account needed to play)</Typography>
-                        
-                        </Stack>
+                />
+            </Box>
+        </div>
+    )
+}
 
-                    </Box>
-                </Box>
-            </div>
+
+export const Home = () => {
+    return (
+        <>
+            <Container maxWidth="lg">
+            <FormContainer>        
+                <Typography variant="h2" component="h2" > Welcome!</Typography>
+                <Typography variant="body" component="body" align="center"> Please Enter Play Code</Typography>
+                <TextField required id="shopTag" type="shopTag" label="Shop Tag" autoComplete="off"/>
+                <TextField required id="digitKey" type="digitKey" label="Digit Key" autoComplete="off" />                
+                <Stack spacing={10} direction="column">
+            
+                <Button variant="outlined" color ="secondary" type="submit"  >Enter</Button>
+                <Typography variant="overline" component="overline" align="center"> (No account needed to play)</Typography>
+                
+                </Stack>
+            </FormContainer>      
 
             <Grid container justifyContent="space-between"> 
             <Box
