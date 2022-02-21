8147d7cd515f7c37d53987c43874eaf0bc5dcfdb
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-02-20 21:57:29 -0800

    refactor: suppressed the console warning about auth emulator being enabled

--- src/firebase.js
+++ src/firebase.js
@@ -12,9 +12,18 @@ export const functions = getFunctions(firebaseApp);
 export const db = getFirestore(firebaseApp);
 export const AuthContext = createContext();
 
+// A toggle for auth emulator warnings (they tend to bloat the unit test logs)
+const consoleInfo = console.info
+function setShowAuthEmulatorWarning(show) {
+    console.info = show ? consoleInfo : () => { }
+}
+
 // Connect to emulators if running on localhost
 if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
     connectFunctionsEmulator(functions, "localhost", 5001);
     connectFirestoreEmulator(db, "localhost", 8080);
+
+    setShowAuthEmulatorWarning(false);
     connectAuthEmulator(getAuth(), "http://localhost:9099");
+    setShowAuthEmulatorWarning(true);
 }
