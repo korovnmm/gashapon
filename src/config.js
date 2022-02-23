03db0c98e15927d8893f2c034ebd2807f55d6a6b
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-02-23 11:56:38 -0800

    undo 7d35a16

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: "gashapon-40f57", //process.env.REACT_APP_PROJECT_ID,
+    projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
