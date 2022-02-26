4cbf060d30b8ae137b818638162125169cb9353f
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-02-26 15:05:29 -0800

    refactor(ci): changed base url from localhost to 127.0.0.1

--- src/tests/e2e.test.js
+++ src/tests/e2e.test.js
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer'
 
 const isHeadless = true;
-const BASE_URL = "http://localhost:5000"
+const BASE_URL = "http://127.0.0.1:5000" //"http://localhost:5000"
 
 const email = "somebody@example.com";
 const password = "somebody'sPassword123";
