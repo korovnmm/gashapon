13bf372a1df7584542d65470507aa025dcdf0e35
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-03-06 22:43:22 -0800

    feat(ci)!: added a unit test for prize creation.
    	- requires a new function to be implemented in db/index.js: getPrizeInfo(id)

--- src/tests/api.test.js
+++ src/tests/api.test.js
@@ -4,6 +4,7 @@ import {
     getAuth, 
     signOut
 } from 'auth'
+import { getPrizeInfo } from 'db'
 
 // Setup
 let auth = getAuth();
@@ -26,7 +27,8 @@ describe('Server', () => {
 
 
 
-// Main Tests
+// --- Tests ---
+
 describe('Shopkeeper', () => {
 
     it("can create a new user account", () => {
@@ -76,6 +78,45 @@ describe('Shopkeeper', () => {
                     expect(result.data[key]["email"]).toMatch("customer2@example.com");
                     expect(result.data[key]["memo"]).toMatch("");
                 }
+            });
+    });
+
+    /**
+     * This test expects 2 documents to have been created: one in 'prizes', and one in 'prize-info',
+     * the call to addNewPrize should return data from the document that was created in 'prizes'.
+     */
+    it("can add prizes via functions endpoint", () => {
+        const prizeData = {
+            description: "It's really cool!",
+            image: "assume we have uploaded an image earlier, its url goes here",
+            name: "Cool Prize #1",
+            quantity: 11
+        };
+
+        return call("addNewPrize", prizeData)
+            .then((result) => {
+                let keys = Object.keys(result.data); 
+                expect(keys.length).toBe(1); // should have only returned 1 prize object
+                
+                // Should have these keys: createdAt, creatorUserID, quantity
+                let prizeID = keys[0];
+                let prize = result.data[prizeID];
+                expect(prize).toMatchObject({
+                    createdAt: expect.any(String),
+                    creatorUserID: auth.currentUser.uid,
+                    quantity: prizeData.quantity
+                });
+                expect(Object.keys(prize).length).toBe(3); // only 3 variables in the dictionary
+                
+                // Verify that the corresponding prize-info document has been created
+                // Should have these keys: description, image, name, lastModified
+                let infoData = getPrizeInfo(prizeID);
+                expect(infoData).toMatchObject({
+                    name: prizeData.name,
+                    description: prizeData.description,
+                    image: prizeData.image,
+                    lastModified: expect.any(String)
+                });
             });
     });
 
