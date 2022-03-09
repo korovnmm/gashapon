252f125a8a7c2dfc93e51b88da2f1e7e87e8e73c
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-03-09 15:25:10 -0800

    refactor(firebase): changed firestore port to 8081

--- src/firebase.js
+++ src/firebase.js
@@ -23,7 +23,7 @@ function setShowAuthEmulatorWarning(show) {
 // Connect to emulators if running on localhost
 export function connectFirebaseEmulators() {
     connectFunctionsEmulator(functions, "localhost", 5051);
-    connectFirestoreEmulator(db, "localhost", 8080);
+    connectFirestoreEmulator(db, "localhost", 8081);
     connectStorageEmulator(storage, "localhost", 9199);
 
     setShowAuthEmulatorWarning(false);
