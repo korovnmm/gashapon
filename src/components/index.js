17b014c4d1ff5c0941f2e2fae7b1ae9787bbe340
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-03-08 17:45:42 -0800

    refactor(home): moved ResponsiveAppBar to it's own jsx file under the components folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { AuthenticatedRoute, UnauthenticatedRoute } from './AuthRoutes';
 export { default as GoogleIcon } from './GoogleGLogo';
+export { default as ResponsiveAppBar } from './ResponsiveAppBar';
