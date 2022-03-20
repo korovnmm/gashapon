d25823cc7aec2501472eecb5f3d6178a981802b5
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-03-19 17:27:20 -0700

    feat! modified Dashboard.js, added Dashboard.jsx and dashboard.scss

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,10 @@
 export { AuthenticatedRoute, UnauthenticatedRoute } from './AuthRoutes';
+
+export { 
+    DashboardContainer,
+    DashNavbar, 
+    LinkTab 
+} from './Dashboard';
+
 export { default as GoogleIcon } from './GoogleGLogo';
 export { default as ResponsiveAppBar } from './ResponsiveAppBar';
