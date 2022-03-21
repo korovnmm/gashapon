69776833ab93566ca4ed88e2f6faea751641dbf0
My Linh Quach <mylinhq@Mys-MacBook-Pro.local>
2022-03-20 18:00:58 -0700

    display image and upload to firestore, not through data grid yet

--- src/pages/dashboard/Inventory.js
+++ src/pages/dashboard/Inventory.js
@@ -5,15 +5,13 @@ import {
     TextField,
 } from '@mui/material'
 import { useCallback, useEffect } from 'react'
-import {firebaseApp} from 'firebase'
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-
-import { DataGrid } from '@mui/x-data-grid'
 import { addNewPrize } from 'api'
+import { DataGrid } from '@mui/x-data-grid'
 import { useAuthState } from 'auth'
 import { getPrizesGeneratedByUser, savePrizesToMemory } from 'db';
 import { useState } from 'react'
-
+import {storage} from 'storage'
+import {uploadBytes, ref,getDownloadURL } from 'firebase/storage'
 //const amountOptions = Array.from({length:10}, (v,k)=>k+1); 
 
 const columns = [
@@ -22,23 +20,39 @@ const columns = [
     { field: 'description', headerName: 'Description', width: 170 },
     { field: 'createdAt', headerName: 'Date added', width: 220, type: "dateTime",
     valueGetter: ({value}) => value && (new Date(value.seconds*1000)) },
-    { field: 'quantity', headerName: 'Quantity', width: 90},
-    //{ field: 'image', headerName: 'Quantity', width: 90} take in var image that correlates w the product in the image ID
-
+    { field: 'quantity', headerName: 'Quantity', type: 'number', editable: true, width: 90},
+    {  field: 'image',
+        headerName: 'Image',
+        width: 150,
+        renderCell: (params) => <img src={params.value} />}
 ];
-
 
 export const Inventory = () => {
     const { user } = useAuthState();
     const [rows, setRows] = useState([]);
     const [open, setOpen] = useState(false); // snackbar state 
-    
+ 
     useEffect(() => {
         getPrizesGeneratedByUser(user) 
             .then((prizeData) => {
                 setRows(prizeData)
-            });
+                
+            })
+        ;
     }, [rows, user]);
+
+    const [selectedImage, setSelectedImage] = useState(null);
+    // const imageUpload = (event) =>{
+    //     console.log(event.target.files[0]);
+    //     const image = event.target.files[0];
+    //     const imageRef = ref(storage,image.name)
+    //     uploadBytes(imageRef, image).then((snapshot) => {
+    //         console.log('Uploaded file!');
+        
+    //       });
+    //     return imageRef
+    //     }
+
 
     const handleClose = (event, reason) => { // snackbar close
         if (reason === 'clickaway') {
@@ -46,72 +60,38 @@ export const Inventory = () => {
         }
         setOpen(false);
     };
+    
 
     const sendNewPrizeRequest = useCallback(async e => {
         e.preventDefault();
         
+        const url = getDownloadURL(imageRef);
+
         const { name, description, quantity } = e.target.elements;
-        addNewPrize(name.value, description.value, quantity.value)
+        addNewPrize(name.value, description.value, quantity.value,)
             .then((result) => {
                 const data = result.data;
                 const prizes = []
                 for(const [key, value] of Object.entries(data)){
                     value.code = key;
                     prizes.push(value);
+                    prizes.push(url);
                 }
                 setRows(savePrizesToMemory(prizes));
+        
                 setOpen(true);
             })
             .catch((error) => {
                 const code = error.code;
                 const message = error.message;
-                const details = error.details;
                 alert(`${code}: ${message}`);
             });
     }, []);
-    
-    //grabs our firebase storage
-    const storage = getStorage(firebaseApp);
 
-    //on upload of image, handle the file and upload it to the firebase storag using uploadbytes
-    const fileselectorHandler = (e) =>{
-            
-            const file = e.target.files[0]
-            const storageRef = ref(storage)
-            const imageRef = ref(storage,file.name)
-            const imageName = imageRef.name;
 
-            uploadBytes(imageRef, file).then((snapshot) => {
-                console.log('Uploaded file!');
-
-              });
-
-            // const storageRef = app.storage().ref();
-            // const imageRef = storageRef.child(file.name);
-        // imageRef.getDownloadURL().then(function(url){
-                
-            //     document.querySelector('img').src = url;
-                
-            //   }).catch(function(error) {
-            //     console.error(error);
-            //   });
-            
-        //  storageRef.child(imageRef).getDownloadURL().then(function(url) {
-        //      var test = url;
-        //      alert(url);
-        //      document.querySelector('img').src = test;
-
-        //  }).catch(function(error) {
-
-        //  });
-        }
-            
-   
-
- 
     return (
         <>
-        
+
             <div class="prizes-header"></div>
             <div class="prizes-body">
                 <DataGrid
@@ -120,25 +100,37 @@ export const Inventory = () => {
                     columns={columns}
                     pageSize={10}
                     rowsPerPageOptions={[10]}
-                    checkboxSelection
                 />
-                
-
             </div>
-            
             <Box component="form" class="prizes-footer" onSubmit={sendNewPrizeRequest}>
-                <form>
-                <input type="file" onChange ={fileselectorHandler}/>
-                            
+                    {selectedImage && (
+                        <div>
+                        <img alt="not found" width={"250px"} src={URL.createObjectURL(selectedImage)} />
+                        <br />
+                        <button onClick={()=>setSelectedImage(null)}>Remove</button>
+                        </div>
+                    )}
+                    <input
+                        type="file"
+                        name="myImage"
+                        onChange={(event) => {
+                            const image = event.target.files[0];
+                            const imageRef = ref(storage,image.name)
+                            console.log(event.target.files[0]);
+                            setSelectedImage(event.target.files[0]);
+                            uploadBytes(imageRef, image).then((snapshot) => {
+                            console.log('Uploaded file!');
+                        });
+        }}
+      />
+
                 <TextField required id="name" type="name" label="Prize Name" />
                 <TextField id="description" type="text" label="Prize Description" />
                 <TextField required id="quantity" type="quantity" label="Quantity" />
-                
-                <Button  variant="contained" type="submit">Add Prize!</Button>
-                </form>
 
+                <Button  variant="contained" type="submit">Add Prize!</Button>
             </Box>
             <Snackbar message="Prize successfully generated" open={open} autoHideDuration={6000} onClose={handleClose}/>
         </>
     );
-}
+    }
