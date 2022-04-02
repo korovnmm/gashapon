8cfd8a9b32529ce7e096170b1239c21b13b88937
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-04-02 14:49:28 -0700

    Merge changes from story44-inventorypage (#81)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import {
-  BrowserRouter as Router,
   Redirect,
-  Route,
-  Link,
+  Route
 } from 'react-router-dom'
 import { StyledEngineProvider } from '@mui/material'
-import { 
-  getAuth, 
-  signOut, 
-  AuthContextProvider 
-} from './auth'
-import { AuthenticatedRoute, UnauthenticatedRoute } from './components'
+import { AuthenticatedRoute, NavBar, UnauthenticatedRoute } from './components'
 
 import { Home } from './pages/home/Home'
 import { About } from './pages/About'
@@ -22,28 +15,17 @@ import { RedeemScreen } from './pages/home/RedeemScreen'
 function App() {
   return (
     <StyledEngineProvider injectFirst>
-    <AuthContextProvider>
-      <Router>
-        <header>
-          <div class="navbar-main">
-            <Link to="/" id="nav-item"> Home </Link> |
-            <Link to="/about" id="nav-item"> About </Link> |
-            <Link to="/login" id="nav-item"> Login </Link> |{' '}
-            <Link to="/signup" id="nav-item"> SignUp </Link> |{' '}
-            <Link to="/signup" id="nav-item" onClick={() => signOut(getAuth())}> SignOut </Link>
-          </div>
+      <header>
+        <NavBar />
+      </header>
 
-        </header>
-        
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/redeem"><Redirect to={'/'} /></Route>
-        <Route path="/redeem/:shopTag/:code" component={RedeemScreen} />
-        <AuthenticatedRoute path="/dashboard" component={Dashboard} />
-        <UnauthenticatedRoute exact path="/login" component={Login} />
-        <UnauthenticatedRoute exact path="/signup" component={SignUp} />
-      </Router>
-    </AuthContextProvider>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/redeem"><Redirect to={'/'} /></Route>
+      <Route path="/redeem/:shopTag/:code" component={RedeemScreen} />
+      <AuthenticatedRoute path="/dashboard" component={Dashboard} />
+      <UnauthenticatedRoute exact path="/login" component={Login} />
+      <UnauthenticatedRoute exact path="/signup" component={SignUp} />
     </StyledEngineProvider>
   );
 }
---
+++ src/pages/dashboard/Inventory.js
@@ -0,0 +1,159 @@
+import { DataGrid } from '@mui/x-data-grid'
+import { 
+    Box,
+    Button,
+    Snackbar,
+    TextField,
+    Avatar
+} from '@mui/material'
+import { 
+    useCallback, 
+    useEffect,
+    useState
+} from 'react'
+
+import { addNewPrize } from 'api'
+import { useAuthState } from 'auth'
+import {
+    getPrizesGeneratedByUser,
+    savePrizesToMemory,
+    deletePrize
+} from 'db'
+import { 
+    uploadImage
+} from 'storage'
+
+
+const columns = [
+    {field: 'image', headerName: 'Image', width: 150,
+        renderCell: (params) => {
+            return (<Avatar src={params.value}/>);
+        }
+    },
+    { field: 'name', headerName: 'Item', width: 170 },
+    { field: 'description', headerName: 'Description', width: 170 },
+    { field: 'createdAt', headerName: 'Date added', width: 220, type: "dateTime",
+        valueGetter: ({value}) => value && (new Date(value.seconds*1000)) },
+    { field: 'quantity', headerName: 'Quantity', type: 'number', width: 90}
+];
+
+export const Inventory = () => {
+    const { user } = useAuthState();
+    const [rows, setRows] = useState([]);
+    const [open, setOpen] = useState(false); // snackbar state 
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedRows, setSelectedRows] = useState([])
+   
+    useEffect(() => {
+        getPrizesGeneratedByUser(user) 
+            .then((prizeData) => {
+                setRows(prizeData) 
+            });
+    }, [rows, user]);
+
+    
+    const imageUpload = useCallback((event) => {
+        const image = event.target.files[0];
+        if (image.type !== "image/png" && image.type !== "image/jpeg") {
+            alert("Invalid file type! Must be png or jpeg.")
+            return;
+        }
+        setSelectedImage(image);
+    }, []);
+
+
+    const handleClose = (event, reason) => { // snackbar close
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+    
+
+    const sendNewPrizeRequest = useCallback(async e => {
+        e.preventDefault();
+        const { name, description, quantity } = e.target.elements;
+        
+        // Upload image
+        const url = await uploadImage(selectedImage);
+        if (!url) { // if url is null
+            alert("Failed to upload image!");
+            return;
+        }
+          
+        // TODO: make sure user is passing a valid integer value
+        
+        addNewPrize(name.value, description.value, quantity.value, url)
+            .then((result) => {
+                result = result.data;
+                const fullPrizeData = {
+                    docId: result.id,
+                    name: result.prizeInfoData.name,
+                    description: result.prizeInfoData.description,
+                    image: result.prizeInfoData.image,
+                    quantity: result.prizeMetaData.quantity,
+                    createdAt: result.prizeMetaData.createdAt
+                }                
+                setRows(savePrizesToMemory([fullPrizeData]));
+                setOpen(true);
+            })
+            .catch((error) => {
+                const code = error.code;
+                const message = error.message;
+                alert(`${code}: ${message}`);
+            });
+    }, [selectedImage]);
+
+
+    const onDeleteClick = useCallback(async (e) => {
+        let numDeleted = 0;
+        for(let i = 0; i < selectedRows.length; i++) {
+            const row = selectedRows[i] - numDeleted - 1; // index 0 refers to row 1 in the DataGrid
+            const id = rows[row].docId;
+            
+            if (await deletePrize(id))
+                numDeleted++;
+            else
+                console.error("Failed to delete prize with ID: ", id);
+        }
+        setRows(rows.filter(index => !selectedRows.includes(index)));
+    }, [rows, selectedRows]);
+
+
+    // HTML
+    return (
+        <>
+            <div class="prizes-header"></div>
+            <div class="prizes-body">
+                <DataGrid
+                    autoHeight={true}
+                    rows={rows}
+                    columns={columns}
+                    pageSize={10}
+                    rowsPerPageOptions={[10]}
+                    checkboxSelection
+                    onSelectionModelChange={(ids) => {setSelectedRows(ids)}}
+                />
+            </div>
+            
+            <Box component="form" class="prizes-footer" onSubmit={sendNewPrizeRequest}>
+                <Button variant="contained" color="primary" onClick={onDeleteClick} startIcon={<div/>}>Delete</Button>
+                <input
+                    accept="image/*"
+                    type="file"
+                    name="myImage"
+                    onChange={imageUpload}
+                    required
+                />
+
+                <TextField required id="name" type="name" label="Prize Name" />
+                <TextField id="description" type="text" label="Prize Description" />
+                <TextField required id="quantity" type="quantity" label="Quantity" />
+
+                <Button  variant="contained" type="submit">Add Prize!</Button>
+            </Box>
+
+            <Snackbar message="Prize successfully generated" open={open} autoHideDuration={6000} onClose={handleClose}/>
+        </>
+    );
+}
