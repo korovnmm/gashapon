e81f2422586a7344d837a24533a49b77e6c0bdd2
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-04-15 15:39:21 -0700

    refactor(ci): wrapped render-dependant tests with an act() callback

--- src/tests/Inventory.test.js
+++ src/tests/Inventory.test.js
@@ -18,25 +18,29 @@ let auth = getAuth();
 let exampleUserEmail = "inventory@example.com";
 let examplePassword = "inventoryPassword123";
 beforeAll(() => { // create a new user account and navigate to the inventory page
-    return createUserWithEmailAndPassword(auth, exampleUserEmail, examplePassword)
-        .then(() => {
-            return call("userLoggedIn")
-                .then((json) => {
-                    expect(json.data.result).toMatch("created");
-                    
-                    const user = auth.currentUser;
-                    render(
-                        <MemoryRouter initialEntries={["/dashboard"]}>
-                            <AuthContextProvider value={{ user }}>
-                                <App />
-                            </AuthContextProvider>
-                        </MemoryRouter>
-                    );
-                });
+    return act(async () => {
+        return createUserWithEmailAndPassword(auth, exampleUserEmail, examplePassword)
+            .then(() => {
+                return call("userLoggedIn")
+                    .then((json) => {
+                        expect(json.data.result).toMatch("created");
+                        
+                        const user = auth.currentUser;
+                        render(
+                            <MemoryRouter initialEntries={["/dashboard"]}>
+                                <AuthContextProvider value={{ user }}>
+                                    <App />
+                                </AuthContextProvider>
+                            </MemoryRouter>
+                        );
+                    });
+            });
         });
 });
 afterAll(() => {
-    return auth.currentUser.delete();
+    return act( () => {
+        return auth.currentUser.delete();
+    });
 });
 
 
