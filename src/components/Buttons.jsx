9eb8f2e0970d58144a820b8a2f999b61c866c6d4
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-05-02 12:33:07 -0700

    feat: added new reusable button components

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,14 +2,39 @@ import {
     Button
 } from '@mui/material';
 
+export function OutlinedButton(props) {
+    return (<SubmitButton type="button" {...props} />);
+}
+
 export function SubmitButton(props) {
     return (
         <Button 
-            className="submit-button"
+            className="submit-button katta-button"
             variant="outlined" 
             color="secondary"
             type="submit"  
             {...props} 
+        />
+    );
+}
+
+export function TextButton(props) {
+    return (
+        <Button 
+            className="text-button katta-button"
+            variant="text"
+            {...props}
+        />
+    );
+}
+
+export function SolidButton(props) {
+    return (
+        <Button
+            disableElevation
+            className="solid-button katta-button"
+            variant="contained"
+            {...props}
         />
     );
 }
