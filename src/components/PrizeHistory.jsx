8a199a891da59d582dd839dda0177d4fba3b365c
korovnmm <66383716+korovnmm@users.noreply.github.com>
2022-05-04 22:33:21 -0700

    feat: pruned PrizeHistory.jsx and implemented a slider

--- src/components/PrizeHistory.jsx
+++ src/components/PrizeHistory.jsx
@@ -1,4 +1,3 @@
-import { Box, Container, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useContext, useEffect, useState, } from "react";
 import { PrizeContext } from "pages/home/RedeemScreen";
@@ -10,14 +9,46 @@ import {
     where,
 } from 'firebase/firestore'
 import { getPrizeInfo } from "db";
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-//getticketsbyemail
-//similar to get tickets by user
+import Slider from "react-slick";
+import SliderArrow from "./SliderArrow";
+
+const sliderSettings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 8,
+    slidesToScroll: 4,
+    nextArrow: <SliderArrow />,
+    prevArrow: <SliderArrow />,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        }
+    ]
+};
+
+
 /**
- * @param {*} user 
- * @returns a list of prizes (info and metadata together) created by the given user
+ * @param {string} email 
+ * @returns a list of tickets containing the matching email field
  */
 const getTicketsByEmail = async (email) => {
     const ticketRef = collection(db, "ticket-info");
@@ -32,17 +63,14 @@ const getTicketsByEmail = async (email) => {
         const doc = snap.docs[i];
         let ticketData = doc.data();
         ticketData.id = i + 1;
+        ticketData.docID = doc.id;
         redeemedTickets.push(ticketData);
-
     }
     return redeemedTickets;
 }
-//prizeList[i].image set it as a state, 
-//state for rendering, 
 
 export function PrizeHistoryCarousel(props) {
     const [prizeList, setPrizeList] = useState([]);
-    const [ticketList, setTicketList] = useState([]);
     const prize = useContext(PrizeContext);
 
     useEffect(() => {
@@ -50,53 +78,40 @@ export function PrizeHistoryCarousel(props) {
             const ticketList = await getTicketsByEmail(prize.ticket.email);
             let prizeList = [];
             for (let ticket of ticketList){
-                prizeList.push(await getPrizeInfo(ticket.prizeID));
+                let prizeInfo = await getPrizeInfo(ticket.prizeID);
+                prizeInfo.ticketURL = ticket.docID.replace('-', '/');
+                prizeList.push(prizeInfo);
             }
             setPrizeList(prizeList);
-            setTicketList(ticketList);
         }
         if (prize.ticket.email)
             fetchData();
     }, [prize.ticket.email]); // gets called everytime variable updates, else once.
     
     return (
-        <Container maxWidth="lg">
-            <Box height = "10%" width = "50%" component="center" 
-            alignItems="center" justifyContent="center" display = "flex"
-            sx={{ borderRadius: "2%", backgroundColor: "white"}}>
-                <Container maxWidth="sm">
-                
-                <ImageList cols={prizeList.length}>
+        <center id="prizehistory-box" className="carousel">
+                <Slider {...sliderSettings}>
                     {prizeList.map((prize) => (
                         
-                            <ImageListItem key={prize.img} >
-                                <Box height = "100%" width = "100%" sx={{ borderRadius: "2%", backgroundColor: "gray"}}>
-                                <Link to= "/reedem">
+                        <div className="carousel-item" key={prize.ticketURL}>
+                            <Link to={`/redeem/${prize.ticketURL}`}>
                                 <img
                                     width = "100"
                                     height = "100"
-                                    objectFit = 'cover'
+                                    objectfit = 'cover'
                                     src={prize.image}
                                     srcSet={prize.image}
                                     alt={prize.name}
-                                    loading="lazy"
-                                    style ={{borderRadius: "5%"}}>
+                                    loading="lazy" />
+                            </Link>
 
-                                    </img>
-                                 </Link>
-                                </Box>
+                            <p style={{margin: "0"}}>
+                                {prize.name}
+                            </p>
+                        </div>
 
-                            <Box sx={{ borderRadius: "2%", backgroundColor: "black"}}>
-                                <Typography variant="caption" color="common.white" style={{ display: "inline-block", whiteSpace: "pre-line" }}>
-                                    {prize.name}
-                                </Typography>
-                            </Box>
-                            </ImageListItem>
                     ))}
-                
-                </ImageList>
-                </Container>
-            </Box>
-            </Container>
+                </Slider>
+        </center>
     );
 }
