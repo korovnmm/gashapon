110771071df09ad355fb6fd20e6b3eed7de8e512
adrienne.slawik <79721937+adrienne-slawik@users.noreply.github.com>
2022-05-05 18:32:35 -0700

    Styles for Redeem's MachineBox component + mp4

--- src/components/MachineBox.jsx
+++ src/components/MachineBox.jsx
@@ -7,11 +7,29 @@ export function MachineBox(props) {
     /*
      * Once cranking has finished, you should call props.onCranked()
      * to display the prize pop-up
+     * 
+     * 
+     * src = {}?
      */
     
     return (
-        <Box component="center" sx={{backgroundColor:"white"}}>
-            <h1>MachineBox</h1>
+        <Box component="center">
+            <div id="machine-box-wrap">
+                <h1 class="machine-box-header">Click to spin!</h1>
+                <div id="machine-inbox">
+                    <img id="machine-img" onclick="changeImage()" src="./assets/kattapon_spin.mp4"></img>
+                </div>
+            </div>
+
         </Box>
     );
+
+    function changeImage() {
+        if (document.getElementById("machine-img").src == "./assets/kattapon_spin.mp4"){
+            document.getElementById("machine-img").src = "./assets/kattapon_spin.mp4";
+        } else {
+            document.getElementById("machine-img").src = "./assets/kattapon_spin.mp4";
+        }
+        props.onCranked();
+    }
 }
